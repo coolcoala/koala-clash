828a0d3b2c5eeb8a0e244b8d1485f9869748f92f
Ivan Kolesnikov <b1svixyz@mail.ru>
2026-03-04 21:04:33 +0700

    Update Koala Clash to version 1.0.2 and refactor naming conventions
    - Changed product name references from "Koala-Clash" to "Koala Clash" and "koala-clash" to "koala.clash" across various files for consistency.
    - Updated package version in PKGBUILD files for koala-clash, koala-clash-bin, koala-clash-electron, koala-clash-electron-bin, koala-clash-git, and koala-clash-electron-git.
    - Modified the post-install script to reflect the new launcher name.
    - Adjusted electron-builder.yml to use the new naming conventions and updated artifact naming.
    - Ensured that all relevant paths and configurations are aligned with the new naming scheme.

--- aur/koala-clash-electron-bin/koala-clash.sh
+++ aur/koala-clash-electron-bin/koala-clash.sh
@@ -3,10 +3,10 @@
 XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-~/.config}
 
 # Allow users to override command-line options
-if [[ -f "${XDG_CONFIG_HOME}/koala-clash-flags.conf" ]]; then
-	mapfile -t MIHOMO_PARTY_USER_FLAGS <<<"$(grep -v '^#' "${XDG_CONFIG_HOME}/koala-clash-flags.conf")"
+if [[ -f "${XDG_CONFIG_HOME}/koala.clash-flags.conf" ]]; then
+	mapfile -t MIHOMO_PARTY_USER_FLAGS <<<"$(grep -v '^#' "${XDG_CONFIG_HOME}/koala.clash-flags.conf")"
 	echo "User flags:" ${MIHOMO_PARTY_USER_FLAGS[@]}
 fi
 
 # Launch
-exec electron /opt/koala-clash ${MIHOMO_PARTY_USER_FLAGS[@]} "$@"
+exec electron /opt/koala.clash ${MIHOMO_PARTY_USER_FLAGS[@]} "$@"
--- aur/koala-clash/koala-clash.sh
+++ aur/koala-clash/koala-clash.sh
@@ -3,10 +3,10 @@
 XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-~/.config}
 
 # Allow users to override command-line options
-if [[ -f "${XDG_CONFIG_HOME}/koala-clash-flags.conf" ]]; then
-	mapfile -t MIHOMO_PARTY_USER_FLAGS <<<"$(grep -v '^#' "${XDG_CONFIG_HOME}/koala-clash-flags.conf")"
+if [[ -f "${XDG_CONFIG_HOME}/koala.clash-flags.conf" ]]; then
+	mapfile -t MIHOMO_PARTY_USER_FLAGS <<<"$(grep -v '^#' "${XDG_CONFIG_HOME}/koala.clash-flags.conf")"
 	echo "User flags:" ${MIHOMO_PARTY_USER_FLAGS[@]}
 fi
 
 # Launch
-exec /opt/koala-clash/koala-clash ${MIHOMO_PARTY_USER_FLAGS[@]} "$@"
+exec /opt/koala.clash/koala.clash ${MIHOMO_PARTY_USER_FLAGS[@]} "$@"
